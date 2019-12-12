97ecfdafd8430e2549b9a7e5aba7ac24308f9277
jhosep7 <jodia.ms@outlook.com>
2019-12-12 14:05:00 -0500

    2-args.py2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
         print("{:d} arguments.".format(argc - 1))
     else:
         if argc == 2:
-            print("{:d} argument:".format(argc -1))
+            print("{:d} argument:".format(argc - 1))
         else:
-            print("{:d} arguments:".format(argc -1))
+            print("{:d} arguments:".format(argc - 1))
     for i in range(1, argc):
         print("{:d}: {:s}".format(i, argv[i]))
