294b1e2bce43f5c414b4b11c0f1236268cd6b904
jhosep7 <jodia.ms@outlook.com>
2019-12-12 22:03:39 -0500

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-from sys import argv
-from calculator_1 import add, sub, mul, div
-argc = len(argv)
-if argc < 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-elif argv[2] != "+" and argv[2] != '-' and argv[2] != "*" and argv[2] != '/':
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
-else:
-    a = int(argv[1])
-    b = int(argv[3])
-    if argv[2] == "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
-        exit(0)
-    if argv[2] == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-        exit(0)
-    if argv[2] == "*":
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-        exit(0)
-    if argv[2] == '/':
-        print("{} / {} = {}".format(a, b, div(a, b)))
-        exit(0)
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    argc = len(argv)
+    Sign = argv[2]
+    if argc != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if Sign != "+" and Sign != "-" and Sign != "*" and Sign != "/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        if argv[2] == "+":
+            print("{} + {} = {}".format(a, b, add(a, b)))
+            exit(0)
+        if argv[2] == "-":
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+            exit(0)
+        if argv[2] == "*":
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+            exit(0)
+        if argv[2] == "/":
+            print("{} / {} = {}".format(a, b, div(a, b)))
+            exit(0)
