c855f936ee98d0ccc2c9a1f4be05f3c0e814768a
jhosep7 <jodia.ms@outlook.com>
2019-12-17 17:13:58 -0500

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    x = ""
+    Myvar = ""
     for i in range(len(my_string)):
         if (my_string[i] != 'c' and my_string[i] != 'C'):
-            x += my_string[i]
-    return  x
+            Myvar += my_string[i]
+    return  Myvar
