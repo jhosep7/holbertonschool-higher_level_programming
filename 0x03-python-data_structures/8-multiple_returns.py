2bd1dc7e77dfd2cbe96afadd902b0b6262c90d46
jhosep7 <jodia.ms@outlook.com>
2019-12-17 17:31:07 -0500

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     Tot = len(sentence)
-    if Tot > 0:
-        MyChar = sentence[0]
-    else:
-        "None"
+    MyChar = sentence[0] if Tot > 0 else "None"
     NewOne = Tot, Mychar
     return(NewOne)
