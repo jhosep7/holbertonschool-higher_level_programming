7dfae15f74ec3a3b526225fa7f0b7e632b2626fe
jhosep7 <jodia.ms@outlook.com>
2019-12-19 14:26:05 -0500

    9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for Key, Value in a_dictionary.items():
-        Key: Value * 2
-    return a_dictionary
+    CpyDict = {key: value * 2 for key, value in a_dictionary.items()}
+    return CpyDict
