4323451e49e18e86c01f0186f4aa760dd4231ca8
jhosep7 <jodia.ms@outlook.com>
2020-01-09 17:41:03 -0500

    3-square.py and 4-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 class Square:
+    """
+        Square Class with a private instance
+            size: the size of the square
+    """
     def __init__(self, size=0):
+        """
+        Arguments:
+            size: the size of the square
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
         return (self.__size * self.__size)
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+class Square:
+    """Square Class with a private instance
+    """
+    def __init__(self, size=0):
+        """
+        Arguments:
+            size: the size of the square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size< 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    """GET
+    """
+    @property
+    def size(self):
+        return (self.__size)
+    """SET
+    """
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return (self.__size ** 2)
