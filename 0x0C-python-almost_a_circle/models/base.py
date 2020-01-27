220add8d6ee583fbef05fa68a3c164a92609dae3
jhosep7 <jodia.ms@outlook.com>
2020-01-27 18:55:38 -0500

    adding comments

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
+"""class base
+"""
 import json
 import turtle
 from random import choice as random
 import csv
-"""class base
-"""
 
 
 class Base:
-
+    """class constructor definition to check for id
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-
+        """checks for id
+        """
         if id is not None:
             self.id = id
         else:
@@ -21,12 +23,16 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """returns the json string rep of list_dictionaries
+        """
         if list_dictionaries is None:
             list_dictionaries = []
         return (json.dumps(list_dictionaries))
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """writes the json string rep of list_objs to a file
+        """
         TheFile = cls.__name__ + ".json"
         with open(TheFile, mode="w") as MyFile:
             if list_objs is None:
@@ -37,6 +43,8 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        """Returns the list of the json string represenation
+        """
         if json_string is None or len(json_string) == 0:
             return ([])
         else:
@@ -44,6 +52,8 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
+        """Returns an instance with all attributes already set
+        """
         if cls.__name__ is "Rectangle":
             Aux = cls(1, 1)
         if cls.__name__ is "Square":
@@ -53,6 +63,8 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
+        """Returns a list of instances
+        """
         TheFile = str(cls.__name__) + ".json"
         try:
             with open(TheFile, mode="r") as JsonFile:
@@ -63,6 +75,8 @@ class Base:
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
+        """Serializes in csv
+        """
         TheFile = cls.__name__ + ".csv"
         with open(TheFile, mode="w", newline="") as CSVfile:
             if list_objs is None or list_objs == []:
@@ -78,6 +92,8 @@ class Base:
 
     @classmethod
     def load_from_file_csv(cls):
+        """Deserializes from csv
+        """
         try:
             TheFile = cls.__name__ + ".csv"
             with open(TheFile, newline="") as CSVfile:
@@ -93,6 +109,8 @@ class Base:
 
     @staticmethod
     def draw(list_rectangles, list_squares):
+        """creates square and rectangle on GUI
+        """
         InkColor = ('blue', 'black', 'red')
         for R in list_rectangles:
             Lines = turtle.Turtle()
