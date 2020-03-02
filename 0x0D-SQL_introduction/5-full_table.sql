42a31ff7201e366470a0b3e15abfa82548bc5ef9
jhosep7 <jodia.ms@outlook.com>
2020-03-02 14:11:11 -0500

    tasks 5 to 9

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Full description
+-- script that prints full description of the table first_table.
+SHOW CREATE TABLE first_table;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Count 89
+-- script that displays the number of records with id = 89.
+SELECT COUNT(id) FROM first_table WHERE id = '89';
