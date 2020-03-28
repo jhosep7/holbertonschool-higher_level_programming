519f808d0b5bc236edaf85ff4c35c992121ee138
Jose Diaz <jodia.ms@outlook.com>
2020-03-28 18:52:59 -0400

    fix

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -5,14 +5,15 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
- 
+
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Sect = sessionmaker(bind=engine)
     Base.metadata.create_all(engine)
-    Var = Sect().query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    Var = Sect().query(State).filter(State.name.like('%a%')).order_by(
+        State.id).all()
     if Var:
         for i in Var:
             if 'a' in i.name:
