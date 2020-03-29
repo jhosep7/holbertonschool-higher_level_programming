8395c14276ba6a472ad6c859369113ef24e2ea7a
Jose Diaz <jodia.ms@outlook.com>
2020-03-29 10:02:38 -0400

    14-model_city_fetch_by_state.

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""contains the class definition of a City"""
-from sqlalchemy import Column, Integer, ForeignKey, String
-from sqlalchemy.orm import relationshipg
-from model_state import Base, State
+"""contains the class definition of a City."""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class City(Base):
-    """Inheritance from State"""
-    __tablename__ = 'cities'
-    id = Column('id', Integer, primary_key=True, nullable=False)
+    """City class"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
