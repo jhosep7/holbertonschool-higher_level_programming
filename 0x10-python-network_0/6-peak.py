53df59d45eca695b4b85958ee6e3af6528f55197
jhosep7 <jodia.ms@outlook.com>
2020-04-09 12:13:39 -0500

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,11 +8,11 @@ def find_peak(list_of_integers):
     if not arr:
         return None
     if len(arr) > 1:
-        if arr[0] > arr[1]:
+        if arr[0] >= arr[1]:
             return arr[0]
-        if arr[-1] > arr[-2]:
+        if arr[-1] >= arr[-2]:
             return arr[-1]
-    for i in range(len(arr)):
-        if arr[i] > arr[i-1] and arr[i] > arr[i+1]:
-            return arr[i]
+        for i in range(len(arr)):
+            if arr[i] > arr[i-1] and arr[i] > arr[i+1]:
+                return arr[i]
     return arr[0]
