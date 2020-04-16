268f2877aad5d570b389d7544646016439e2d888
jhosep7 <jodia.ms@outlook.com>
2020-04-15 19:13:02 -0500

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     q = argv[1] if len(argv) > 1 else ""
     try:
         R1 = requests.post('http://0.0.0.0:5000/search_user',
-                           data={'q':q}).json()
+                           data={'q': q}).json()
         print("[{}] {}".format(R1['id'], R1['name'])
               if 'id' in R1 and 'name' in R1 else "No result")
     except ValueError:
