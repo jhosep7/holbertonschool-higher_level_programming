d8dcfa769733ab7a03910c07e841e9b28bfafe0c
jhosep7 <jodia.ms@outlook.com>
2020-04-15 19:33:19 -0500

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,5 +5,5 @@ import requests
 
 if __name__ == "__main__":
     R1 = requests.get(argv[1])
-    R2 = R1.status_code
-    print("Error code: {}".format(R2) if R2 > 400 else R1.text)
+    code = R1.status_code
+    print("Error code: {}".format(code) if code > 400 else R1.text)
