ab9793ea4c4e61a7564b9600146a028f14af49c5
jhosep7 <jodia.ms@outlook.com>
2020-04-15 19:51:58 -0500

    task 7 & 8

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,8 +6,5 @@ from sys import argv
 
 if __name__ == "__main__":
     R1 = requests.get(argv[1])
-    code = R1.status_code
-    if code > 400:
-        print("Error code: {}".format(code))
-    else:
-        print(reply.text)
+    R2 = R1.status_code
+    print("Error code: {}".format(R2) if R2 > 400 else R1.text)
