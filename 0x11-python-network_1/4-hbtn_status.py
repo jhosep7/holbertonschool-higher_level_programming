e823c71fc2ae1bedd213345c123f62ce5626a733
jhosep7 <jodia.ms@outlook.com>
2020-04-15 19:07:01 -0500

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,7 @@
 import requests
 
 if __name__ == "__main__":
-    Ans = requests.get ('https://intranet.hbtn.io/status').text
+    Ans = requests.get('https://intranet.hbtn.io/status').text
     print("Body response:")
     print("\t- type: {}".format(type(Ans)))
-    print("\t- content: {}:".format(Ans))
+    print("\t- content: {}".format(Ans))
